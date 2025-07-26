227fcf9a3fe5029d9fb7a0f1b850f84439bcec7b
Miguel Flores-Acton <floresacton@gmail.com>
2025-07-25 23:21:30 -0400

    Update build extras

--- build.sh
+++ build.sh
@@ -31,7 +31,8 @@ echo "DEPS: ${DEPS}"
 for ARCH in "${ARCHS[@]}"; do
     BUILD_DIR="packages/$ARCH"
     CONTROL_DIR="$BUILD_DIR/DEBIAN"
-    DIST_DIR="dists/$DIST/$COMPONENT/binary-$ARCH"
+    DIST_DIR="dists/$DIST/$COMPONENT"
+    BIN_DIR="$DIST_DIR/binary-$ARCH"
     DEBFILE="${PKGNAME}_${VERSION}_${ARCH}.deb"
 
     mkdir -p "$CONTROL_DIR"
@@ -47,26 +48,35 @@ Depends: $DEPS
 Description: $DESCRIPTION
 EOF
 
-    mkdir -p "${BUILD_DIR}/${ARCH}/DEBIAN"
     dpkg-deb --build "$BUILD_DIR"
-    cp "${BUILD_DIR}.deb" "$POOL_DIR/$DEBFILE"
+    mv "${BUILD_DIR}.deb" "$POOL_DIR/$DEBFILE"
 
-    mkdir -p "$DIST_DIR"
-    dpkg-scanpackages -m pool /dev/null | gzip -9 > "$DIST_DIR/Packages.gz"
+    mkdir -p "$BIN_DIR"
+    dpkg-scanpackages -m pool /dev/null | gzip -9 > "$BIN_DIR/Packages.gz"
 
-    cat > "$DIST_DIR/Release" <<EOF
+    cat > "$BIN_DIR/Release" <<EOF
 Archive: $DIST
 Component: $COMPONENT
 Origin: Local
 Label: $PKGNAME repo
 Architecture: $ARCH
 EOF
+    
+    mkdir "$BIN_DIR/c-n-f"
+    touch "$BIN_DIR/Translation-en" \
+        "$BIN_DIR/Translation-en_US" \
+        "$BIN_DIR/Components" \
+        "$BIN_DIR/c-n-f/Metadata"
 
 done
 
-rm "${BUILD_DIR}.deb"
+ICON_DIR="$DIST_DIR/Icons"
+mkdir "$ICON_DIR"
+touch "$ICON_DIR/48x48" \
+touch "$ICON_DIR/64x64" \
+touch "$ICON_DIR/64x64@2"
 
 #gpg --default-key "$GPG_KEY" --armor --detach-sign -o "$DIST_DIR/Release.gpg" "$DIST_DIR/Release"
 #gpg --default-key "$GPG_KEY" --clearsign -o "$DIST_DIR/InRelease" "$DIST_DIR/Release"
 
-echo "Package built and placed at $POOL_DIR/$DEBFILE"
+echo "Packages built"
